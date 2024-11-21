7f0a9bcc62555b2947aa5bff250412501cd270ba
Tereza Konstari <teresa.konstari@gmail.com>
2024-11-20 22:10:50 -0800

    updating model

--- patient_surveillance_model.py
+++ patient_surveillance_model.py
@@ -24,8 +24,6 @@ units = tf.config.experimental.list_physical_devices('GPU')
 for unit in units:
     tf.config.experimental.set_memory_growth(unit, True)
 
-tf.config.list_physical_devices('GPU')
-
 """# 2. Remove dodgy images"""
 
 import cv2
@@ -87,9 +85,7 @@ validation_size = int(len(data)*.2)+1
 test_size = int(len(data)*.1)+1
 
 #train_size
-
 #validation_size
-
 #test_size
 
 train = data.take(train_size)
@@ -99,23 +95,27 @@ test = data.skip(train_size+validation_size).take(test_size)
 """# 6. Build Deep Learning Model"""
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 
-model = Sequential()
+seq_model = Sequential()
 
-model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
-model.add(MaxPooling2D())
-model.add(Conv2D(32, (3,3), 1, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Conv2D(16, (3,3), 1, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
+#Deep Learning model layers
+#relu -> to convert negative output to 0
+#sigmoid -> to get outcome 0 or 1
+seq_model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
+seq_model.add(MaxPooling2D())
+seq_model.add(Conv2D(32, (3,3), 1, activation='relu'))
+seq_model.add(MaxPooling2D())
+seq_model.add(Conv2D(16, (3,3), 1, activation='relu'))
+seq_model.add(MaxPooling2D())
+seq_model.add(Flatten())
+seq_model.add(Dense(256, activation='relu'))
+seq_model.add(Dense(1, activation='sigmoid'))
 
-model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+#adam keras optimizer
+seq_model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
 
-model.summary()
+#seq_model.summary()
 
 """# 7. Train"""
 
@@ -123,20 +123,20 @@ logdir='logs'
 
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
-hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
+histr = seq_model.fit(train, epochs=20, validation_data=validation, callbacks=[tensorboard_callback])
 
 """# 8. Plot Performance"""
 
 fig = plt.figure()
-plt.plot(hist.history['loss'], color='teal', label='loss')
-plt.plot(hist.history['val_loss'], color='orange', label='val_loss')
+plt.plot(histr.history['loss'], color='green', label='loss')
+plt.plot(histr.history['val_loss'], color='orange', label='validation loss')
 fig.suptitle('Loss', fontsize=20)
 plt.legend(loc="upper left")
 plt.show()
 
 fig = plt.figure()
-plt.plot(hist.history['accuracy'], color='teal', label='accuracy')
-plt.plot(hist.history['val_accuracy'], color='orange', label='val_accuracy')
+plt.plot(histr.history['accuracy'], color='blue', label='accuracy')
+plt.plot(histr.history['val_accuracy'], color='yellow', label='validation accuracy')
 fig.suptitle('Accuracy', fontsize=20)
 plt.legend(loc="upper left")
 plt.show()
@@ -151,7 +151,7 @@ acc = BinaryAccuracy()
 
 for batch in test.as_numpy_iterator():
     X, y = batch
-    yhat = model.predict(X)
+    yhat = seq_model.predict(X)
     pre.update_state(y, yhat)
     re.update_state(y, yhat)
     acc.update_state(y, yhat)
@@ -170,7 +170,7 @@ resize = tf.image.resize(img, (256,256))
 plt.imshow(resize.numpy().astype(int))
 plt.show()
 
-yhat = model.predict(np.expand_dims(resize/255, 0))
+yhat = seq_model.predict(np.expand_dims(resize/255, 0))
 
 yhat
 
@@ -183,7 +183,7 @@ else:
 
 from tensorflow.keras.models import load_model
 
-model.save(os.path.join('models','imageclassifier.h5'))
+seq_model.save(os.path.join('models','imageclassifier.h5'))
 
 new_model = load_model('imageclassifier.h5')
 
